fae93aea6fb849554842350656acef48b1161c6e
JNotelddim <JNotelddim@users.noreply.github.com>
2023-10-31 16:08:59 -0700

    Reformat

--- when-broke.sh
+++ when-broke.sh
@@ -25,15 +25,15 @@ echo "... Current pwd: $(pwd)"
 
 echo "... Validating that current directory is a git project..."
 echo "$ git status ..."
-exec git status
+git status
 
 if [ $? -eq 0 ]; then
-    echo "Command Executed Successfully"
+    echo "[Command Executed Successfully]"
 else
-    echo "Command Failed"
+    echo "[Command Failed]"
 fi
 
-echo "Fin."
+echo "__ Fin. __"
 
 
 
@@ -42,59 +42,44 @@ echo "Fin."
 # _____________________________
 
 
-cat <<END
-# Long form notes
-_______________________
-
-## Goal: given a yarn package script which is not working with the latest changes,
-get the most recent commit where the script was functioning.
-
-### Input:
-- script to run
-- last working commit [initial commit of repo if none]
-
-### Assumptions:
-- This script runs git commands in the correct working directory with the correct
-git account information.
-
-### Steps:
-
-1. Check current working directory,
-  a) validate that it's git directory,
-  b) validate that there is an authenticated git user (?)
-
-2. Check out the latest on `main` (or other developmentBranch).
-
-3. Check that the selected script is in fact, a script, and not working.
-  a) Try running it, expect it to fail.
-
-4. Validate that the selected script did in fact work at the "last working commit".
-  a) try checking out the commit, (might fail)
-  b) run the script, expect it to succeed.
-
-5. Search for the most recent commit where the script was working.
-
-....
-
-### Improvements:
-- load variables (script, last working commit, working directory) from args or
-  from a config file if no args are provided.
-- search alg efficiencies
-- breaking commit: squash commit recursion?
-- compile a platform-agnostic executable: make it packageable into npm?
-END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# cat <<END
+# # Long form notes
+# _______________________
+#
+# ## Goal: given a yarn package script which is not working with the latest changes,
+# get the most recent commit where the script was functioning.
+#
+# ### Input:
+# - script to run
+# - last working commit [initial commit of repo if none]
+#
+# ### Assumptions:
+# - This script runs git commands in the correct working directory with the correct
+# git account information.
+#
+# ### Steps:
+#
+# 1. Check current working directory,
+#   a) validate that it's git directory,
+#   b) validate that there is an authenticated git user (?)
+#
+# 2. Check out the latest on `main` (or other developmentBranch).
+#
+# 3. Check that the selected script is in fact, a script, and not working.
+#   a) Try running it, expect it to fail.
+#
+# 4. Validate that the selected script did in fact work at the "last working commit".
+#   a) try checking out the commit, (might fail)
+#   b) run the script, expect it to succeed.
+#
+# 5. Search for the most recent commit where the script was working.
+#
+# ....
+#
+# ### Improvements:
+# - load variables (script, last working commit, working directory) from args or
+#   from a config file if no args are provided.
+# - search alg efficiencies
+# - breaking commit: squash commit recursion?
+# - compile a platform-agnostic executable: make it packageable into npm?
+# END
